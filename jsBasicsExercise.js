8d8743af57d710a23411a54f1a9bab5e60fb53b8
Chelsea <cdl8790@gmail.com>
2021-02-22 13:07:19 -0600

    Complete 1st

---
+++ jsBasicsExercise.js
@@ -0,0 +1,51 @@
+// 1
+console.log(7);
+
+// 2
+// 5 Main Data types:
+/* Numbers, Strings, Booleans, Null, Undefined */
+
+// 3.
+// 6 Operators:
+// Add, Subtract, Muliplying, Dividing, Exponentiation, Modulus.
+
+// 4
+console.log(5%2);
+console.log(5%4);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL ');
+
+// 6.
+console.log("Wanda" + "Vision");
+
+// 7.
+console.log("Danielle".length + "Logan".length);
+
+// 8
+console.log("Avengers:Endgame"[15]);
+
+// Bonus
+// 1B. 
+console.log('\\');
+
+// 2B
+console.log(5**4-9);
+
+// 3B
+console.log("I".length - ("Am".length - "Iron".length) - "Man".length);
+
+// 4B 
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5B
+var m = 10;
+var c = 5;
+var u = m + c;
